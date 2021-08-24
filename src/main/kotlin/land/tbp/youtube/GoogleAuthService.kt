21c9df5d7000c8764721b60d719949f2225dba2d
TheBestPessimist <cristian@tbp.land>
2021-08-24 11:24:42 +0300

    almost got it working

---
+++ src/main/kotlin/land/tbp/youtube/GoogleAuthService.kt
@@ -0,0 +1,145 @@
+package land.tbp.land.tbp.youtube
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.api.client.auth.oauth2.*
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse
+import com.google.api.client.googleapis.util.Utils
+import com.google.api.client.http.HttpExecuteInterceptor
+import com.google.api.client.http.HttpRequest
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.util.store.DataStore
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.youtube.YouTubeScopes
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.bufferedReader
+
+@Suppress("LocalVariableName", "PrivatePropertyName")
+class GoogleAuthService(
+    private val httpTransport: NetHttpTransport,
+    private val googleCredentialsRepository: GoogleCredentialsRepository
+) {
+    private val JSON_FACTORY: JsonFactory = Utils.getDefaultJsonFactory()
+    private val AUTH_SCOPES = mutableListOf(YouTubeScopes.YOUTUBE_READONLY, "https://www.googleapis.com/auth/userinfo.email")
+
+    private val credentialDataStore: DataStore<StoredCredential> = FileDataStoreFactory(File("config/private")).getDataStore("oauthCredentials") // todo probably i should get rid of this data store, as i will persist the tokens in memory.
+
+    @Suppress("RedundantSamConstructor")
+    private val googleAuthorizationCodeFlow: GoogleAuthorizationCodeFlow by lazy {
+        val CLIENT_SECRETS = "C:/work/Youtube Subscriptions Mailer/config/private/client_secret.json" // todo from eng
+
+        val oAuth2ClientSecret = GoogleClientSecrets.load(JSON_FACTORY, Paths.get(CLIENT_SECRETS).bufferedReader())
+
+//        oAuth2ClientSecret.
+
+        GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, oAuth2ClientSecret, AUTH_SCOPES)
+            .setCredentialDataStore(credentialDataStore)
+            .setAccessType("offline")
+            .setCredentialCreatedListener(AuthorizationCodeFlow.CredentialCreatedListener { _: Credential?, tokenResponse: TokenResponse? ->
+                println("onTokenResponse")
+                println(tokenResponse)
+
+                if (tokenResponse is GoogleTokenResponse) {
+                    val email = GoogleIdToken.parse(JSON_FACTORY, tokenResponse.idToken).payload.email
+                    googleCredentialsRepository.newCredentials(GoogleCredentials(email, tokenResponse))
+                }
+
+
+            })
+            .addRefreshListener(object : CredentialRefreshListener {
+                override fun onTokenResponse(credential: Credential?, tokenResponse: TokenResponse?) {
+                    println("onTokenResponse")
+                    println(tokenResponse)
+
+                    // TODO tbp: update credentials
+
+                }
+
+                override fun onTokenErrorResponse(credential: Credential?, tokenErrorResponse: TokenErrorResponse?) {
+                    println("onTokenErrorResponse")
+                    println(credential)
+                    println(tokenErrorResponse)
+
+                    // TODO tbp: what to do if refresh fails?
+                }
+            })
+            .build()
+    }
+
+    fun authorizeNewUser(): Credential {
+
+
+        AuthorizationCodeInstalledApp.DefaultBrowser()
+        return AuthorizationCodeInstalledApp(googleAuthorizationCodeFlow, LocalServerReceiver()).authorize("")
+    }
+
+    fun forUser(user: String): Credential {
+        val tokenResponse: TokenResponse = googleCredentialsRepository.getByUser(user)
+        return createCredentialWithRefreshToken(tokenResponse)
+    }
+
+
+
+    private fun createCredentialWithRefreshToken(tokenResponse: TokenResponse?): Credential {
+        return Credential.Builder(BearerToken.authorizationHeaderAccessMethod())
+            .setTransport(httpTransport)
+            .setJsonFactory(JSON_FACTORY)
+//            .setTokenServerUrl(GenericUrl("https://server.example.com/token"))
+//            .setClientAuthentication(BasicAuthentication("s6BhdRkqt3", "7Fjfp0ZBr1KtDRbnfVdmIw"))
+            .setClientAuthentication(object : HttpExecuteInterceptor {
+                override fun intercept(request: HttpRequest?) {
+                    TODO("Not yet implemented")
+                }
+            })
+            .build()
+            .setFromTokenResponse(tokenResponse)
+    }
+}
+
+
+class GoogleCredentialsRepository {
+    private val storage = Paths.get("C:/work/Youtube Subscriptions Mailer/config/private/credentials.json")
+
+    private val credentials: MutableList<GoogleCredentials> by lazy {
+
+        val credentialsFromPersistence: MutableList<GoogleCredentials> = jacksonObjectMapper().enable(DeserializationFeature.USE_LONG_FOR_INTS).readValue(storage.toFile())
+//        if (credentialsFromPersistence.isNotEmpty()) {
+            credentialsFromPersistence
+//        } else {
+//        mutableListOf()
+//        }
+    }
+
+    private val JSON_FACTORY: JsonFactory = Utils.getDefaultJsonFactory()
+
+
+    fun newCredentials(googleCredentials: GoogleCredentials) {
+        credentials.add(googleCredentials)
+        dumpCredentials()
+
+
+    }
+
+    private fun dumpCredentials() {
+        val json = jacksonObjectMapper().writeValueAsBytes(credentials)
+        Files.write(storage, json)
+    }
+
+    fun getByUser(user: String): TokenResponse {
+        return credentials.find { it.email == user }!!.googleTokenResponse
+    }
+}
+
+data class GoogleCredentials(
+    val email: String,
+    val googleTokenResponse: GoogleTokenResponse
+)
---
+++ src/main/kotlin/land/tbp/youtube/main.kt
@@ -0,0 +1,78 @@
+package land.tbp.land.tbp.youtube
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.services.youtube.YouTube
+import com.google.api.services.youtube.model.SubscriptionListResponse
+import java.security.GeneralSecurityException
+
+
+/**
+ * Explanation for Parts and Fields: https://developers.google.com/youtube/v3/getting-started#partial
+ *
+ * For testing, i want
+ * - parts: all (i think i have to type the parts manually, but maybe there's an "ALL" value)
+ * - fields: `*`
+ *
+ * TODO: try to use etags to maximize performance, if possible: https://developers.google.com/youtube/v3/getting-started#performance
+ */
+
+
+val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
+val googleAuthService = GoogleAuthService(httpTransport, GoogleCredentialsRepository())
+val youtubeService = YoutubeService(httpTransport)
+
+/**
+ * Call function to create API service object. Define and
+ * execute API request. Print API response.
+ *
+ * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
+ */
+fun main(args: Array<String>) {
+//    newUser(ChannelIDs.Subscriptions.public1)
+//    newUser(ChannelIDs.Subscriptions.private)
+
+    existingUser("cristian.nahsuc@gmail.com")
+//    existingUser("cristian@tbp.land")
+
+}
+
+fun existingUser(user: String) {
+    val credential = googleAuthService.forUser(user)
+    val youtube: YouTube = youtubeService.createForUser(credential)
+
+    val subscriptions: SubscriptionListResponse = youtube.subscriptions()
+        .list("snippet,contentDetails")
+        .setPrettyPrint(true)
+        .setMine(true)
+//        .setChannelId(channelId)
+        .setFields("*")
+        .execute()
+
+    println(subscriptions)
+}
+
+private fun newUser(channelId: String) {
+    val credential = googleAuthService.authorizeNewUser()
+    val youtube: YouTube = youtubeService.createForUser(credential)
+
+    val subscriptions: SubscriptionListResponse = youtube.subscriptions()
+        .list("snippet,contentDetails")
+        .setPrettyPrint(true)
+        .setChannelId(channelId)
+        .setFields("*")
+        .execute()
+
+    println(subscriptions)
+}
+
+
+
+
+
+object ChannelIDs {
+    object Subscriptions {
+        const val public1 = "UC5cw1VFE1rsZbN9Q2cA40aw"
+        const val public2 = "UCdCaX6gDODXvM5r7bHickww"
+        const val private = "UC2Zm9eTC_AaM040N98mRIrA"
+    }
+}
