1e9448963c4c06efc4c69723f0322dd197728051
TheBestPessimist <cristian@tbp.land>
2022-10-15 15:42:08 +0300

    OAuth example using Google Client.
    Not working as expected because `urn:ietf:wg:oauth:2.0:oob` is not longer accepted by google, so Jetbrains must update their Ktor  documentation :(

---
+++ src/main/kotlin/land/tbp/google/oauth/Google Client OAuth Example.kt
@@ -0,0 +1,91 @@
+package land.tbp.land.tbp.google.oauth
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.auth.*
+import land.tbp.config
+import land.tbp.land.tbp.youtube.ErrorInfo
+import land.tbp.land.tbp.youtube.GoogleUserInfo
+
+
+/*
+Documentation here: https://ktor.io/docs/bearer-client.html#oauth-flow
+ */
+suspend fun main() {
+
+    val bearerTokenStorage = mutableListOf(
+        BearerTokens(
+            "aya29.a0Aa4xrXNfppcDiHoyJMjzJ4Oxo-FAGMApkTTXHEYXu4Ai8qw-3T4przPzaC3IEUJ5_rSiTzHmds9YVS3DzLSLrWEWLIc66d8OI59WK4xpcwoV2ZzcC5CMiBZJ1bkel5n-qCTETSE7yGZ8Cdlk_FCCkdgREzeyaCgYKATASARESFQEjDvL96VXrF5Y1B5n6rMbZH1XT8w0163",
+            "1//09TzHCj7FPv-xCgYIARAAGAkSNwF-L9IrhIH7de4XHKIaytX6p5xyJjj3wxdjziFzPLi0vOC6Gbfz-0PVp08TPnAxqL47CQbCWiM"
+        )
+    )
+
+    val client = HttpClient(Apache) {
+        install(Logging) { level = LogLevel.ALL }
+        install(ContentNegotiation) {
+            jackson {
+                disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
+            }
+        }
+        install(Auth) {
+            bearer {
+                loadTokens { bearerTokenStorage.last() }
+                /*
+                Technical: By default, the client will not send the credentials on the first request.
+                Instead, the client makes the very first request unauthenticated.
+                - if it receives a 401 (Unauthorized), then it sends the authorization in every subsequent request
+                - if it does not receive a 401, then it will not send the authorization and continue making requests unauthorized
+
+                Therefore Ktor has added the feature `sendWithoutRequest`, which returns a boolean:
+                 - Should I send the auth credentials when making this particular request? yes, no
+                If we return true from the lambda, then we send the credentials to ALL requests and ALL domains.
+                If we do a check like `it:Request -> request.url.host == "www.googleapis.com"`, this means we return true only for google APIs
+                => we send credentials only for googleapis, and not for any other domains.
+                Why has Ktor decided to not auth by default, I don't know, but at least you now know what this function does :^).
+                 */
+                sendWithoutRequest { true }
+                refreshTokens {
+//                    println("Will refresh token")
+                    val newAccessToken = client.submitForm(
+                        url = "https://accounts.google.com/o/oauth2/token",
+                        formParameters = Parameters.build {
+                            append("grant_type", "refresh_token")
+                            append("client_id", config.googleCredentials.clientId)
+                            append("client_secret", config.googleCredentials.clientSecret)
+                            append("refresh_token", oldTokens?.refreshToken ?: "")
+                        },
+                        block = { markAsRefreshTokenRequest() }
+                    )
+                        .body<OAuthAccessTokenResponse.OAuth2>()
+
+//                    println("newAccessToken: $newAccessToken")
+                    bearerTokenStorage.add(BearerTokens(newAccessToken.accessToken, oldTokens?.refreshToken!!))
+                    bearerTokenStorage.last()
+                }
+            }
+        }
+    }
+
+    val response: HttpResponse = client.get("https://www.googleapis.com/oauth2/v2/userinfo")
+    try {
+        val userInfo: GoogleUserInfo = response.body()
+        println("UserInfo response: $userInfo")
+        println("Hello, ${userInfo.name}!")
+    } catch (e: Exception) {
+        val errorInfo: ErrorInfo = response.body()
+        println(errorInfo.error.message)
+    }
+}
