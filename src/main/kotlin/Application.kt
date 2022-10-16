9b349f32e5273ac30d50a6fb2d4f48c65d943474
TheBestPessimist <cristian@tbp.land>
2022-10-16 15:23:03 +0300

    Move config to its own package

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,8 +2,6 @@ package land.tbp
 
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.google.api.services.youtube.YouTubeScopes
-import com.sksamuel.hoplite.ConfigLoader
-import com.sksamuel.hoplite.parsers.PropsParser
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.plugins.*
@@ -21,19 +19,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
 import kotlinx.html.*
-import land.tbp.land.tbp.config.Config
 import land.tbp.land.tbp.youtube.UserCookie
 import land.tbp.land.tbp.youtube.googleOAuthModule
 import org.slf4j.event.Level
 
-val AUTH_SCOPES = mutableListOf(YouTubeScopes.YOUTUBE_READONLY, "https://www.googleapis.com/auth/userinfo.email")
 
-val config: Config = ConfigLoader.builder()
-    .addFileExtensionMapping("env", PropsParser())
-    .build()
-    .loadConfigOrThrow(
-        """C:/work/Youtube Subscriptions Mailer/.env""",
-    )
 
 
 fun main() {
