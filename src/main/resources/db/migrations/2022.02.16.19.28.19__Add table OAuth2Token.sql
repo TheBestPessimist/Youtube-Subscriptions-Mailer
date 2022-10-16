d12a07384fa868f43efde3379e031fc36a058c4a
TheBestPessimist <cristian@tbp.land>
2022-10-16 13:19:13 +0300

    better migration files

--- src/main/resources/db/migrations/2022.02.16.19.28.19__Add table OAuth2Token.sql
+++ src/main/resources/db/migrations/2022.02.16.19.28.19__Add table OAuth2Token.sql
@@ -5,13 +5,13 @@ create table OAuth2Token
             primary key,
     accessToken      text,
     tokenType        text,
-    expiresInSeconds bigint,
+    expiresInSeconds integer,
     refreshToken     text             not null,
     scope            text,
     user_id          INTEGER NOT NULL
         constraint OAuth2Token_user_user_id_fk
             references user
-);
+) strict ;
 
 create unique index OAuth2Token_OAuth2Token_ID_uindex
     on OAuth2Token (OAuth2Token_ID);
