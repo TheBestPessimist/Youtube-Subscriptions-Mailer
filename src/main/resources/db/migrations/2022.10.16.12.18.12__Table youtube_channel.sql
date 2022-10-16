d30c068187c3e5212b2f1b71edcb996ed548c5e1
TheBestPessimist <cristian@tbp.land>
2022-10-16 13:19:28 +0300

    table youtube_channel

---
+++ src/main/resources/db/migrations/2022.10.16.12.18.12__Table youtube_channel.sql
@@ -0,0 +1,9 @@
+create table main.youtube_channel
+(
+    youtube_channel_id integer not null
+        constraint youtube_channel_pk primary key autoincrement,
+    channel_id         TEXT not null,
+    title              text not null
+)strict ;
+
+create unique index youtube_channel_channel_id_uindex on youtube_channel (channel_id);
